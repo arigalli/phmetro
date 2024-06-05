0c9e8c83420a4b6d73ac9eee1e6677a0bf3640b1
arigalli <arigalli@unr.edu.ar>
2024-06-05 19:32:33 -0300

    Add files via upload
    Sketch de Arduino para manejo de electrodo de pH con placa ph4502C . El sketch realiza 100 mediciones (1 por segundo), cada una de las cuales es el promedio de 500 mediciones instantaneas. Para medir se de oprimir 1 y ejecutar enter

---
+++ phmetrobis.ino
@@ -0,0 +1,49 @@
+/**
+  *  Full example for PH4502C_Sensor.h
+  *  By: Nathanne Isip
+  *  27/06/2023
+  */
+
+
+// Define the pH level pin and temperature pin
+#define ph A0  //ph
+
+float mvfinal,psmedia;
+int arrayps[500];
+char mesage;
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting PH4502C Sensor...");
+    Serial.println("para medir oprima 1");
+    }
+
+void loop() {
+
+  while(Serial.available()>0){
+mesage=Serial.read();  //queda esperando el numero 1 para medir
+Serial.println(mesage);
+if(mesage=='1'){
+      mvfinal=0;
+      for(int n=1;n<100;n++){
+        for(int l=1;l<500;l++){
+        int ps=analogRead(ph);
+        arrayps[l]=ps;
+        }
+        psmedia=0;
+        for(int a=0;a<500;a++){
+        psmedia=psmedia+arrayps[a];
+        }
+        psmedia=psmedia/500;
+      float vph=1000*(psmedia*5.000/1023.000);
+      mvfinal=mvfinal+vph;
+      delay(1000);
+      Serial.println(n);
+      }
+    Serial.print("Medición");  
+    Serial.print(" ");
+    Serial.println(mvfinal/1000);
+}
+Serial.println("para medir oprima 1");
+  }
+}
